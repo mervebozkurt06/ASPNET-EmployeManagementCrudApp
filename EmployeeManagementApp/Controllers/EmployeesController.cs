92d15c0b09889f34bef59b2f35fa499ec80374d1
mervebozkurt06 <mervebzkrts@gmail.com>
2024-01-15 22:47:00 +0300

    Add .vs/ to .gitignore

---
+++ EmployeeManagementApp/Controllers/EmployeesController.cs
@@ -0,0 +1,105 @@
+﻿using EmployeeManagementApp.Data;
+using EmployeeManagementApp.Models;
+using EmployeeManagementApp.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeDbContext employeeDbContext;
+        public EmployeesController(EmployeeDbContext employeeDbContext)
+        {
+            this.employeeDbContext = employeeDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employees = await employeeDbContext.Employees.ToListAsync();
+            return View(employees);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth
+            };
+            await employeeDbContext.Employees.AddAsync(employee);
+            await employeeDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> View(Guid id)
+        {
+            var employee = await employeeDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employee != null)
+            {
+                var viewModel = new UpdateEmployeeViewModel()
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Email = employee.Email,
+                    Salary = employee.Salary,
+                    Department = employee.Department,
+                    DateOfBirth = employee.DateOfBirth
+                };
+                return await Task.Run(() => View("View", viewModel));
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(UpdateEmployeeViewModel model)
+        {
+            var employee = await employeeDbContext.Employees.FindAsync(model.Id);
+            if (employee != null)
+            {
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Salary = model.Salary;
+                employee.Department = model.Department;
+                employee.DateOfBirth = model.DateOfBirth;
+
+                await employeeDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(UpdateEmployeeViewModel model)
+        {
+            var employee = await employeeDbContext.Employees.FindAsync(model.Id);
+
+            if(employee != null)
+            {
+                employeeDbContext.Employees.Remove(employee);
+                await employeeDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EmployeeManagementApp/Data/EmployeeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagementApp.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementApp.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
